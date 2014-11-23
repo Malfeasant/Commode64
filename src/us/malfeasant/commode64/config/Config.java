08a39bc9a00fd5747ce584b0cee5d1e2d2c32594
Mischa Sumrall <madchao@yahoo.com>
2014-11-22 20:36:11 -0700

    bah, hitting a wall again...

--- src/us/malfeasant/commode64/config/Config.java
+++ src/us/malfeasant/commode64/config/Config.java
@@ -1,12 +1,17 @@
 package us.malfeasant.commode64.config;
 
 /**
- * Holds in one place whatever options aren't expected to change in a running system
- * @author Mischa
- *
+ * Holds in one place whatever options aren't expected to change in a running system- 
+ * chip revisions, clock rate...
  */
 public class Config {	// TODO make more meaningful
+	private static Config DEFAULT = new Config();
+	
 	public static Config getDefault() {
-		return new Config();
+		return DEFAULT;
+	}
+	
+	private Config() {
+		
 	}
 }

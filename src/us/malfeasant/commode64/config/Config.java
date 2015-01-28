c006b4fd19f4a4181a39437a4172812126a23581
Malfeasant <mischasumrall@gmail.com>
2015-01-27 20:17:49 -0700

    Dialog cleanup

--- src/us/malfeasant/commode64/config/Config.java
+++ src/us/malfeasant/commode64/config/Config.java
@@ -4,17 +4,19 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 import com.google.gson.Gson;
 
 public class Config {
 	// TODO more options- VIC revision, ROM revisions, etc
-	private final Power power;
-	private final Oscillator oscillator;
+	public final Power power;
+	public final Oscillator oscillator;
 	
 	public static class Builder extends JPanel {
 		private static final long serialVersionUID = 1L;
@@ -24,20 +26,55 @@ public class Config {
 		private Config conf = DEFAULT_CONF;
 		
 		public Builder() {
-			setLayout(new GridBagLayout());
+			super(new GridBagLayout());
 			GridBagConstraints gbc = new GridBagConstraints();
 			gbc.fill = GridBagConstraints.BOTH;
+			//gbc.weightx = 1;
 			gbc.insets = new Insets(2, 2, 2, 2);
 			gbc.gridx = 0;	gbc.gridy = 0;
-			add(new JLabel(Oscillator.class.getSimpleName()), gbc);
+			gbc.gridwidth = GridBagConstraints.REMAINDER;
+			JLabel lab = new JLabel("Easy:");
+			lab.setHorizontalAlignment(SwingConstants.CENTER);
+			add(lab, gbc);
+			gbc.gridwidth = 1;
+			gbc.gridy++;
+			JButton but = new JButton(Oscillator.NTSC.toString());
+			but.addActionListener(event -> makeUS());
+			add(but, gbc);
+			gbc.gridx++;
+			but = new JButton(Oscillator.PAL.toString());
+			but.addActionListener(event -> makeEU());
+			add(but, gbc);
+			gbc.gridx = 0;	gbc.gridy++;
+			gbc.gridwidth = GridBagConstraints.REMAINDER;
+			add(new JPanel(), gbc);	// spacer
+			gbc.gridx = 0;	gbc.gridy++;
+			lab = new JLabel("Hard:");
+			lab.setHorizontalAlignment(SwingConstants.CENTER);
+			add(lab, gbc);
+			gbc.gridwidth = 1;
+			gbc.gridy++;
+			lab = new JLabel(Oscillator.class.getSimpleName());
+			lab.setHorizontalAlignment(SwingConstants.TRAILING);
+			add(lab, gbc);
 			gbc.gridx++;
 			add(oscCombo, gbc);
 			gbc.gridx = 0;	gbc.gridy++;
-			add(new JLabel(Power.class.getSimpleName()), gbc);
+			lab = new JLabel(Power.class.getSimpleName());
+			lab.setHorizontalAlignment(SwingConstants.TRAILING);
+			add(lab, gbc);
 			gbc.gridx++;
 			add(powCombo, gbc);
 			powCombo.setSelectedItem(conf.power);
 			oscCombo.setSelectedItem(conf.oscillator);
+		}
+		private void makeUS() {
+			powCombo.setSelectedItem(Power.US);
+			oscCombo.setSelectedItem(Oscillator.NTSC);
+		}
+		private void makeEU() {
+			powCombo.setSelectedItem(Power.EU);
+			oscCombo.setSelectedItem(Oscillator.PAL);
 		}
 		public Config getConfig() {
 			return conf;
@@ -59,7 +96,6 @@ public class Config {
 			conf = gson.fromJson(s, Config.class);
 		}
 	}
-	
 	
 	private Config(Power pow, Oscillator osc) {
 		power = pow;

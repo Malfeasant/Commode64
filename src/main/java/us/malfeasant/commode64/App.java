6f09cbe2cdf898e0c3ed6735c8b111ebffa5855a
Malfeasant <1212508+Malfeasant@users.noreply.github.com>
2022-08-10 20:46:07 -0700

    this doesn't work well...
    only redraws when mouse is over window- probably threading related since
    drawing happens in bg thread...  also image doesn't resize to fit view,
    can't remember how i got that to work before...

--- src/main/java/us/malfeasant/commode64/App.java
+++ src/main/java/us/malfeasant/commode64/App.java
@@ -2,6 +2,7 @@ package us.malfeasant.commode64;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import us.malfeasant.commode64.machine.Machine;
@@ -17,17 +18,20 @@ public class App extends Application {
         //final Parameters params = getParameters();
         // TODO: more thorough arg parsing
         
+    	var view = new ImageView();
+    	
     	var imp = new Impetus();
     	var machine = new Machine();
     	imp.addCrystalListener(machine);
     	imp.addPowerListener(machine);
+    	machine.bindImageProperty(view.imageProperty());
     	
     	stage.setOnCloseRequest(e -> {
     		// TODO determine if anything should be saved- disk image or similar?
     		imp.shutdown();
     	});
     	
-        var pane = new BorderPane();
+        var pane = new BorderPane(view);
         stage.setScene(new Scene(pane));
         stage.show();
     }
--- src/main/java/us/malfeasant/commode64/machine/Machine.java
+++ src/main/java/us/malfeasant/commode64/machine/Machine.java
@@ -1,5 +1,11 @@
 package us.malfeasant.commode64.machine;
 
+import java.util.Random;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
 import us.malfeasant.commode64.timing.CrystalListener;
 import us.malfeasant.commode64.timing.PowerListener;
 
@@ -8,12 +14,26 @@ import us.malfeasant.commode64.timing.PowerListener;
  * @author Malfeasant
  */
 public class Machine implements CrystalListener, PowerListener {
-	
+	private final ObjectProperty<Image> imageProperty;
+	private final WritableImage image;
+	public Machine() {
+		image = new WritableImage(520, 262);
+		imageProperty = new SimpleObjectProperty<>(image);	// image that we render into		
+	}
+	private final Random rand = new Random();
 	public void powerTick() {
-		// TODO
+		for (int y = 0; y < image.getHeight(); y++) {
+			for (int x = 0; x < image.getWidth(); x++) {
+				image.getPixelWriter().setArgb(x, y, rand.nextInt());
+			}
+		}
 	}
 	
 	public void crystalTick(int howmany) {
 		// TODO
+	}
+	
+	public void bindImageProperty(ObjectProperty<Image> other) {
+		other.bind(imageProperty);
 	}
 }

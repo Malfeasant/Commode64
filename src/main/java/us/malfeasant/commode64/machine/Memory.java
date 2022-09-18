5504eb0d8d5554262c0500b17acf691cf2686a96
Malfeasant <1212508+Malfeasant@users.noreply.github.com>
2022-09-17 23:23:23 -0700

    Getting away from ByteBuffer
    I don't think there's any advantage over plain byte arrays... except
    maybe to implement a custom ByteBuffer just for I/O handling, but I'm
    trying to keep this simple, and it seems like a special case for I/O is
    simpler than ByteBuffers... and while ByteBuffers can be sliced, they
    can't be concatenated together, and without that the slicing doesn't
    really add much.

--- src/main/java/us/malfeasant/commode64/machine/Memory.java
+++ src/main/java/us/malfeasant/commode64/machine/Memory.java
@@ -2,7 +2,7 @@ package us.malfeasant.commode64.machine;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.BooleanProperty;
@@ -16,15 +16,19 @@ import javafx.beans.property.SimpleObjectProperty;
  * @author Malfeasant
  */
 public class Memory {
-	private final ByteBuffer ram;
-	private final ByteBuffer charom;
-	private final ByteBuffer basic;
-	private final ByteBuffer kernal;
-	private final ByteBuffer coloram;	// actually only uses 4 bits per cell, but there is no nybble type...
-	// maybe implement i/o as a custom bytebuffer?  or series of?
+	private final byte[][] ram;	// ram is an array of arrays
+	private final byte[] charom;
+	private final byte[][] basic;
+	private final byte[][] kernal;
+	private final byte[] coloram;	// actually only uses 4 bits per cell, but there is no nybble type...
+	private final byte[] scratch = new byte[0x1000];	// used by ultimax mode to swallow writes to invalid addresses
 	
-	public final ObjectProperty<ByteBuffer> cartLo;	// may or may not be present... 
-	public final ObjectProperty<ByteBuffer> cartHi;
+	private final byte[][] cpureadmap;	// the view of ram from the cpu (64k) for reads (ram, rom, i/o)
+	private final byte[][] cpuwritemap;	// the view of ram from the cpu (64k) for writes (mostly ram)
+	private final byte[][] vicreadmap;	// the view of ram from the vic (16k)
+	
+	public final ObjectProperty<byte[][]> cartLo;	// may or may not be present... 
+	public final ObjectProperty<byte[][]> cartHi;
 	
 	public final BooleanProperty aec;	// Allows CPU to drive the bus- when false, Vic drives it.
 	public final BooleanProperty ba;	// Allows CPU to run- when pulled low, cpu pauses (can finish a write)
@@ -46,13 +50,16 @@ public class Memory {
 	private byte portBits;	// cpu i/o port bits (stored here because otherwise could be lost if direction set to input)
 	private byte portDirection;	// direction for above- true = output
 	
+	boolean cpumapvalid = false;	// false means we need to setup the memory map before next access
+	boolean vicmapvalid = false;	// same as above but for vic
+	
 	public Memory() {
-		ram = ByteBuffer.allocate(0x10000);
-		coloram = ByteBuffer.allocate(0x400);
+		ram = new byte[0x10][0x1000];
+		coloram = new byte[0x400];
 		
-		charom = ByteBuffer.wrap(readFile("chargen", 0x1000)).asReadOnlyBuffer();
-		basic = ByteBuffer.wrap(readFile("basic", 0x2000)).asReadOnlyBuffer();
-		kernal = ByteBuffer.wrap(readFile("kernal", 0x2000)).asReadOnlyBuffer();
+		charom = readFile("chargen", 1)[0];	// only need a single chunk
+		basic = readFile("basic", 2);
+		kernal = readFile("kernal", 2);
 		
 		cartLo = new SimpleObjectProperty<>();
 		cartHi = new SimpleObjectProperty<>();
@@ -72,12 +79,29 @@ public class Memory {
 		loram = new SimpleBooleanProperty(false);
 		hiram = new SimpleBooleanProperty(false);
 		charen = new SimpleBooleanProperty(false);
+		
+		cpureadmap = new byte[0x10][];
+		cpuwritemap = new byte[0x10][];
+		vicreadmap = new byte[4][];
+		
+		// Many signals to watch, but they don't change super often, so will just use invalidation listeners-
+		// Anything touches them, they're invalidated, then all will be recomputed on the next memory access.
+		loram.addListener(p -> cpumapvalid = false);
+		hiram.addListener(p -> cpumapvalid = false);
+		charen.addListener(p -> cpumapvalid = false);
+		ultimax.addListener(p -> {	// if ultimax mode switches, that affects both cpu and vic view of ram
+			cpumapvalid = false;
+			vicmapvalid = false;
+		});
+		
+		va14.addListener(p -> vicmapvalid = false);
+		va15.addListener(p -> vicmapvalid = false);
 	}
 	
-	private byte[] readFile(String name, int expectedBytes) {
+	private byte[][] readFile(String name, int expectedChunks) {
 		byte[] contents;
 		try (var file = ClassLoader.getSystemClassLoader().getResourceAsStream(name)) {
-			if (file.available() == expectedBytes) {
+			if (file.available() == expectedChunks * 0x1000) {
 				contents = file.readAllBytes();
 			} else {
 				// Shouldn't happen...
@@ -92,7 +116,15 @@ public class Memory {
 			System.err.println("Problem reading file: " + name + ".  Aborting.");
 			throw new Error(e);	// TODO recovery?
 		}
-		return contents;
+		var chunks = new byte[expectedChunks][0x1000];
+		if (expectedChunks == 1) {
+			chunks[0] = contents;
+		} else {
+			for (int c = 0; c < expectedChunks; c++) {
+				chunks[c] = Arrays.copyOfRange(contents, c * 0x1000, (c + 1) * 0x1000);
+			}
+		}
+		return chunks;
 	}
 	
 	private byte getHigh(short addr) {	// helper function to read top 4 bits of address
@@ -100,42 +132,72 @@ public class Memory {
 	}
 	
 	private void updateFlags() {	// called whenever i/o port or direction changes
-		loram.set((portBits & portDirection & 1) != 0);
-		hiram.set((portBits & portDirection & 2) != 0);
-		charen.set((portBits & portDirection & 4) != 0);
+		cpumapvalid = false;
+		loram.set((portDirection & 1) != 0 ? (portBits & 1) == 0 : false);	// real circuit has pullup, so reads high if
+		hiram.set((portDirection & 2) != 0 ? (portBits & 2) == 0 : false);	// switched to input- but, these are active
+		charen.set((portDirection & 4) != 0 ? (portBits & 4) == 0 : false);	// low, so meaning is reversed- high = false
 		// TODO cassette data out
 		// TODO cassette switch
 		// TODO cassette motor
 	}
+	
 	/**
 	 * Models a write from CPU- almost always goes to RAM, but may need to be intercepted to go to i/o-
-	 * unclear whether in ultimax mode writes to 1000-7fff go to ram or go nowhere- easy thing would be to handle writes
-	 * normally, but return junk on read- but if ultimax can be entered/exited at will in a running system, that's not
-	 * ideal, we might overwrite ram contents that should be preserved...  so this implementation will block writes
-	 * until I find proof otherwise.
+	 * also, ultimax mode stops writes to everything beyond 0x1000 (except i/o)
 	 * @param addr - Address to write to
 	 * @param data - Data to be written
 	 */
 	public void poke(short addr, byte data) {
-		if ((addr & 1) == addr) {	// either 0 or 1
+		if ((addr & 0xfffe) == 0) {	// i/o port or direction
 			if (addr == 0) {
 				portDirection = data;
-				updateFlags();
 			} else {
 				portBits = data;
-				updateFlags();
 			}
+			updateFlags();
 		} else {
+			if (!cpumapvalid) {
+				// TODO setup memory map
+			}
 			var ha = getHigh(addr);
 			if (ha == 0xd && (ultimax.get() ||	// all ultimax variants have i/o
 					!charen.get() && (!hiram.get() || !loram.get()) )) {	// if no i/o, writes go to ram under char rom
 				// TODO i/o handling
-			} else if (ultimax.get() &&	ha > 0) {	// if not i/o, only first 4k of memory exists in ultimax mode
-				// do nothing
 			} else {
-				ram.put(addr & 0xffff, data);	// need this and otherwise index could be negative...
+				cpuwritemap[ha][addr & 0xfff] = data;
 			}
 		}
+	}
+	
+	private byte readPort() {
+		byte data = 0;
+		if ((portDirection & 1) == 0) {	// input bit
+			if (loram.get()) data |= 1;
+		} else {
+			data |= (portBits & 1);
+		}
+		if ((portDirection & 2) == 0) {	// input bit
+			if (hiram.get()) data |= 2;
+		} else {
+			data |= (portBits & 2);
+		}
+		if ((portDirection & 4) == 0) {	// input bit
+			if (charen.get()) data |= 4;
+		} else {
+			data |= (portBits & 4);
+		}
+		if ((portDirection & 8) == 0) {
+			data |= 8;	// bit will read high if input
+		} else {
+			data |= (portBits & 8);	// read back what was last written
+		}
+		// TODO read cassette switch
+		if ((portDirection & 0x20) == 0) {
+			data |= 0x20;	// bit will read high if input
+		} else {
+			data |= (portBits & 0x20);	// read back what was last written
+		}
+		return data;
 	}
 	
 	/**
@@ -144,42 +206,21 @@ public class Memory {
 	 * @return - Data read from appropriate source
 	 */
 	public byte peek(short addr) {
-		if ((addr & 1) == addr) {	// either 0 or 1
-			if (addr == 0) {
-				return portDirection;
-			} else {
-				byte data = 0;
-				if ((portDirection & 1) == 0) {	// input bit
-					if (loram.get()) data |= 1;
-				} else {
-					data |= (portBits & 1);
-				}
-				if ((portDirection & 2) == 0) {	// input bit
-					if (hiram.get()) data |= 2;
-				} else {
-					data |= (portBits & 2);
-				}
-				if ((portDirection & 4) == 0) {	// input bit
-					if (charen.get()) data |= 4;
-				} else {
-					data |= (portBits & 4);
-				}
-				if ((portDirection & 8) == 0) {
-					data |= 8;	// bit will read high if input
-				} else {
-					data |= (portBits & 8);	// read back what was last written
-				}
-				// TODO read cassette switch
-				if ((portDirection & 0x20) == 0) {
-					data |= 0x20;	// bit will read high if input
-				} else {
-					data |= (portBits & 0x20);	// read back what was last written
-				}
-				return data;
-			}
+		if ((addr & 0xfffe) == 0) {	// i/o port or direction
+			return (addr == 0) ? portDirection : readPort();
 		}
+		if (!cpumapvalid) {
+			// TODO setup memory map
+		}
+		var ha = getHigh(addr);
+		if (ha == 0xd && (ultimax.get() ||	// ultimax always has i/o
+				!charen.get() ||	// as long as char rom isn't enabled, we see i/o
+				(loram.get() && hiram.get() && (!game.get() || exrom.get())))) {	// except for these special cases
+			// TODO special i/o handling
+		}
+		return cpureadmap[ha][addr & 0xfff];
 		//assert (addr == (addr & 0xffff)) : "Problem: address " + addr + " out of range."; don't need this for short addr
-		ByteBuffer buf = null;	// give me exception if I have missed anything
+/*		ByteBuffer buf = null;	// give me exception if I have missed anything
 		if (ultimax.get()) {	// lots of stuff is handled differently, so special case
 			switch (getHigh(addr)) {
 			case 0:
@@ -241,6 +282,29 @@ public class Memory {
 			}
 		}
 		return buf.get(addr);
+*/	}
+	
+	private void cpumap() {	// setup the cpu's view of memory
+		
+	}
+	
+	private void vicmap() {	// setup the video chip's view of memory
+		if (ultimax.get()) {	// vic sees 12k of ram and 2nd half of hi cart rom (but cpu can't write past 4k of ram)
+			for (int i = 0; i < 3; i++) {
+				vicreadmap[i] = ram[i];
+			}
+			vicreadmap[3] = cartHi.get()[1];
+		} else {
+			int ha = (va14.get() ? 4 : 0);
+			ha |= va15.get() ? 8 : 0;
+			for (int i = 0; i < 4; i++) {
+				vicreadmap[i] = ram[i + ha];
+			}
+			if (!va14.get()) {	// special circuit to character generator rom
+				vicreadmap[1] = charom;
+			}
+		}
+		vicmapvalid = true;
 	}
 	
 	/**
@@ -249,16 +313,9 @@ public class Memory {
 	 * @return - 12-bits of data- top 4 bits come from color ram
 	 */
 	public short vread(short addr) {
-		//assert (addr == (addr & 0x3fff)) : "Video read: Address " + addr + " out of range.";
-		var buf = ram;
-		if (va14.get()) addr |= 0x4000;
-		if (va15.get()) addr |= 0x8000;
-		switch (getHigh(addr)) {
-		case 1:
-		case 9:
-			buf = charom;
-			addr &= 0xfff;
-		}
-		return (short) ((coloram.get(addr & 0x3ff) << 8) | buf.get(addr & 0xffff));	// need this and to avoid negative
+		if (!vicmapvalid) vicmap();
+		var ha = getHigh(addr);
+		assert (ha < 4) : "Video read: Address " + addr + " out of range.";
+		return (short) ((coloram[addr & 0x3ff] << 8) | vicreadmap[ha][addr & 0xfff]);
 	}
 }

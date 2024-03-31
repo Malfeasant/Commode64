33e6ecae913d25e9abb52d066a6b2136feced235
Malfeasant <1212508+Malfeasant@users.noreply.github.com>
2024-03-31 14:19:42 -0700

    Demodularize and fix pom so fat jar and javafx:run both work

--- src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module us.malfeasant.commode {
-    requires javafx.controls;
-	requires java.prefs;
-	requires org.tinylog.api;
-    exports us.malfeasant.commode64;
-}
---
+++ src/main/java/us/malfeasant/commode64/Launch.java
@@ -0,0 +1,7 @@
+package us.malfeasant.commode64;
+
+public class Launch {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
